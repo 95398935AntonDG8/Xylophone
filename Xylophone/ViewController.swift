f021ba212763241ef7892e0785ed823c847c4e4f
Your Name <you@example.com>
2018-09-27 15:15:58 -0600

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Anton Miguel de Guia on 2018-09-27.
+//  Copyright © 2018 Anton Miguel de Guia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
