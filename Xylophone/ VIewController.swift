9679848310112db0b417b6b60aa0ce6d0bbd4ad3
Your Name <you@example.com>
2018-10-03 14:31:34 -0600

    third commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -11,7 +11,9 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-    var audioPlayer = AVAudioPlayer()
+    var selectSoundFileName : String = ""
+    var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +23,15 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        selectSoundFileName = soundArray[sender.tag - 1]
+
         playSound()
         
     }
         
     func playSound() {
 
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        let soundURL = Bundle.main.url(forResource: selectSoundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
